58a8db33d508d9e4b8d329cf31d2a03e8f59772c
zafar-03 <zfnanshabhai@gmail.com>
2026-06-03 09:55:10 +0530

    DOM start

---
+++ 3.Document/documentData.js
@@ -0,0 +1,29 @@
+//HTML Full :-
+console.log(document);
+
+
+//How TO Access Head ;
+console.log(document.head);
+
+
+//How TO Access Body ;
+console.log(document.body);
+
+//Error
+// console.log(document.body.h1)
+
+
+
+// How To Access any Element using JS : 
+// 1. using Id : 
+console.log(document.getElementById("heading1"));
+
+
+//2. using Class : 
+console.log(document.getElementsByClassName('para1'));//colletion
+// console.log(document.getElementsByClassName('para1')[2]);
+
+
+//3. using ElementName/Tag name : 
+console.log(document.getElementsByTagName('p'));
+console.log(document.getElementsByTagName('p')[1]);

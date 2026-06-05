f97246a86c10916ac8de2479cafb4f6c79a08ead
zafar-03 <zfnanshabhai@gmail.com>
2026-06-05 09:57:38 +0530

    Variable,Datatype

---
+++ 4.Datatype/datatype.js
@@ -0,0 +1,55 @@
+console.log("Data Type");
+
+
+// 1. Number   : int float
+console.log(12);
+console.log(3.14);
+console.log(typeof(12));
+console.log(typeof(3.14));
+
+// typeof Operator : check type of Data  
+    //  syntax : 1. typeof(data)    2. typeof data
+
+
+// 2. String : char,string
+console.log('Rajesh');
+console.log("Raj");
+console.log(typeof 'A');
+console.log(typeof "Raj");
+
+
+// 3. Boolean : true/false
+console.log(true);
+console.log(typeof true);
+
+
+// 4. Object : Array,object,null
+console.log([11,12,13]);
+console.log({"fname" : "Raj"});
+console.log(null);  
+console.log(typeof null);  
+
+console.log(typeof([11,12,13]));
+console.log(typeof({"fname" : "Raj"}));
+
+
+// Array : 
+// console.log(Array.isArray(12));
+
+
+// 5. Undefined : undefined
+console.log(undefined);
+console.log(typeof undefined);
+
+
+// 6. BigInt : number >
+// bigInt BigInt
+console.log(12345678901234567890n);
+console.log(typeof 12345678901234567890n);
+
+
+// 7. Function : 
+console.log(typeof console.log);
+
+// 8. Symbol
+
---
+++ 5.Variable/variable.js
@@ -0,0 +1,58 @@
+console.log("what is variables understanding data types declaring and initialization variables");
+
+
+// declaration : 
+// in C language : 
+//  intger    int a;
+//  char      char ch;
+
+// in CPP: 
+// string fname;
+
+
+// in JS : 
+// var,let,const
+
+// var data = 12;
+// var data = "Rajesh";
+// let data2 = "Raj";
+// const data3 = "Rajan";
+
+// console.log(data);
+// console.log(typeof data);
+
+// console.log(data2);
+// console.log(typeof data2);
+
+// console.log(data3);
+// console.log(typeof data3);
+
+
+// VAR,LET,CONST : 
+
+// 1. var 
+var a1=11;
+console.log(a1);
+// a1 = 100;
+var a1 = 1000;
+console.log(a1);
+
+
+// 2. let
+let b1=12;
+console.log(b1);
+// b1 = 200;
+// let b1 = 2000;
+// console.log(b1);
+
+
+
+// 3. const
+const c1=13;
+console.log(c1);
+// c1 = 300;
+const c1 = 3000;
+console.log(c1);
+
+
+

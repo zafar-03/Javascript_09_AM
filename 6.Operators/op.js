6c5e16170d71923d8c02a925efc26f5819acc447
zafar-03 <rwr2.mohammadzafar.as@gmail.com>
2026-06-08 10:02:37 +0530

    Operators

---
+++ 6.Operators/op.js
@@ -0,0 +1,77 @@
+// console.log("+ - * / % ++ --");
+// console.log(11 + 12);
+// console.log(11 - 12);
+// console.log(11 * 12);
+// console.log(11 / 12);
+// console.log(25 % 12);   // reminder : 
+//         // 
+// Increment : 
+    // 1. postfix x++
+    // 2. prefix  ++x
+
+// Decrement : 
+    // 1. postfix x--
+    // 2. prefix  --x
+
+
+// console.log("+= -= *= /= %= = ");
+// var a = 12;
+
+// a+=10;// a = a+10
+// a-=10;// a = a-10
+// a*=10;// a = a*10
+// a/=10;// a = a/10
+// a%=10;// a = a%10
+
+
+// console.log(a);
+
+
+// console.log("== != < > <= >= === !==");
+
+// var a1 = 12;
+// var b1 = "12";
+// console.log(a1,typeof a1);
+// console.log(b1,typeof a1);
+
+// console.log(a1==b1);
+// console.log(a1 === b1);
+// console.log(a1 !== b1);
+
+
+
+
+console.log("&& || !");
+
+// AND (&&) : return true/1 if Both Operation are true else return false/0 
+// OR (||) : return false/0 if Both Operation are false else return true/1 
+// NOT (!) : return true if Operation false else return false
+
+// console.log(12>13  && 12!=13);
+// console.log(12>13  || 12!=13);
+
+// console.log(!(12>13));
+
+
+// console.log(11 && 13);
+
+// console.log(false || 0);
+
+
+
+//truthy value :
+// true , 1 , numbers , "sd" ,[] ,{}
+
+
+//falsy value : 
+// false , 0 , "" , null, undefined
+// console.log(Boolean({}));
+
+
+
+console.log(12,typeof 12);
+
+console.log(String(12),typeof String(12));
+
+
+document.querySelector('h1').innerHTML = typeof String(12);

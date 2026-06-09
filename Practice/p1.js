55eb52986f7df8093b62538cfa7df90527cdc56d
zafar-03 <rwr2.mohammadzafar.as@gmail.com>
2026-06-09 09:57:05 +0530

    Practice

---
+++ Practice/p1.js
@@ -0,0 +1,39 @@
+// Ternary Operator :-  
+// Syntax :  (condition) ? st1 : st2;
+
+// number +ve -ve
+// var n1 = -12;
+// (n1>0) ? console.log("+ve") : console.log("-ve");
+
+// age product
+// var age = 15;
+// var product = (age<10) ? "milk" : "tea";
+
+// console.log(product);
+
+
+
+// Function / Methods :
+
+// 1. prompt : 
+// - user input
+// - default return type : string
+// var fname = prompt("Enter Your Name : ");
+// console.log(fname,typeof fname);
+
+// var age = Number(prompt("Enter Your Age : "));
+// console.log(age,typeof age);
+
+
+
+// 2. confirm :
+// - choice 
+// - return boolean true / false
+
+// var yourResponse = confirm("are you sure ?");
+
+// console.log(yourResponse);
+
+
+// 3. alert : 
+// alert("Hello Everyone");

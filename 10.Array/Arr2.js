b0c59d1233424780566c15d3e5313a86e38e6555
zafar-03 <zfnanshbahi@gmail.com>
2026-06-23 09:37:55 +0530

    Array and String Completed

---
+++ 10.Array/Arr2.js
@@ -0,0 +1,73 @@
+// var arr = [11,45,75,21,2,4,88,11,21];
+// console.log(arr);
+//element
+
+// length :   property  
+// return array No element
+// console.log(arr.length);
+
+// indexof :
+// console.log(arr.indexOf(21));
+// console.log(arr.indexOf(100));
+
+
+// lastIndexOf : 
+// console.log(arr.lastIndexOf(21));
+
+
+
+// include :
+// true /false
+// console.log(arr.includes(11));
+
+
+// foreach :
+// console.log("ForEach :");
+// var ans1 = arr.forEach((element)=>{
+//     return element;
+// });
+// console.log(ans1);
+
+// map :
+// console.log("Map :");
+// var ans2 =  arr.map((element)=>{
+//     return element<50;
+// });
+// console.log(ans2);
+
+
+// filter : 
+// console.log("Filter :");
+// var ans3 = arr.filter((element)=>{
+//     return element<50;
+// });
+// console.log(ans3);
+
+
+// every :  true/false
+// var out1 = arr.every((element)=>{
+//     // console.log(element);
+//     return element<100;
+// });
+// console.log(out1);
+
+// some : true/false
+// var out2 = arr.some((element)=>{
+//     console.log(element);
+//     return element>100;
+// });
+// console.log(out2);
+
+// sort :
+// var arr = [11,45,75,21,2,4,88,11,21];
+// console.log(arr);
+
+// arr.sort((a,b)=>{
+//     return a - b;
+// });
+
+// arr.sort((a,b)=>{
+//     return b - a;
+// });
+
+// console.log(arr);
